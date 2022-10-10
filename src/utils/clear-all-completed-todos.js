d99ebaaa2bc7fb28400e9321e8a44e2b30bc9945
Tanusri Ghosh <tghosh25@gmail.com>
2022-10-10 18:58:06 +0530

    Delete clear-all-completed-todos.js

--- src/utils/clear-all-completed-todos.js
+++
@@ -1,9 +0,0 @@
-// Clear all completed todos function
-const clearAllCompletedTodos = (localStorageTodos) => {
-  const localArr = JSON.parse(localStorage.getItem(localStorageTodos));
-  const pendingTodos = localArr.filter((todo) => todo.completed === false);
-
-  localStorage.setItem(localStorageTodos, JSON.stringify(pendingTodos));
-};
-
-export default clearAllCompletedTodos;
